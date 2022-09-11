4950d2990fe6089b396da0f5cb0b3f992ad9a8a1
Marcin <marcinmoch757@gmail.com>
2022-09-11 14:31:45 +0200

    add some comments

--- src/main/java/com/example/emailverificationamigoscode/email/EmailSender.java
+++ src/main/java/com/example/emailverificationamigoscode/email/EmailSender.java
@@ -2,5 +2,5 @@ package com.example.emailverificationamigoscode.email;
 
 public interface EmailSender {
     void send(String to, String email);
-
+//    You dont need this, but is nice cause you can have for instance discord implementations what is really nice!
 }
